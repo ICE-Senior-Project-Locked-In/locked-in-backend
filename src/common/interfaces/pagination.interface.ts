68282939ed4ffcd32d08b42c131ab1efbab36692
TungDude <krittapop.kms@gmail.com>
2026-02-02 14:44:00 +0700

    feat(pagination): pagination helper

---
+++ src/common/interfaces/pagination.interface.ts
@@ -0,0 +1,17 @@
+export interface PaginationOptions {
+    page: number;
+    itemsPerPage: number;
+};
+
+export interface OffsetPaginationOptions {
+    skip: number;
+    take: number;
+}
+
+export interface PaginationMetaData {
+    offset: number;
+    limit: number;
+    total: number;
+    hasNext: boolean;
+    hasPrevious: boolean;
+}
