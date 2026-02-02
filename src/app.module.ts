72913fa3e038c3e7722ef70fdaccc2b60a478eff
TungDude <krittapop.kms@gmail.com>
2026-02-02 14:43:51 +0700

    refactor(user): rename user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { LoggerModule } from './logger/logger.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { UsersModule } from '@/modules/users/users.module';
+import { UserModule } from '@/modules/user/user.module';
 import { NFCModule } from './modules/nfc/nfc.module';
 import { FocusTypeModule } from './modules/focus/focus-type/focus-type.module';
 import { FocusLogModule } from './modules/focus/focus-log/focus-log.module';
@@ -15,7 +15,7 @@ import { UnblockActionModule } from './modules/unblock-action/unblock-action.mod
     LoggerModule,
     DatabaseModule,
     AuthModule,
-    UsersModule,
+    UserModule,
     NFCModule,
     FocusTypeModule,
     FocusLogModule,
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { PrismaService } from "@/database/prisma.service";
 
 @Injectable()
-export class UsersService {
+export class UserService {
     constructor(private readonly prismaService: PrismaService) { }
 
     async getAll() {
